9a1c83ba21893ddcfbd7d1a8bfd4598ea723001f
hoo chong yi <hoochongyi95@gmail.com>
2023-07-24 19:08:11 +0800

    done

--- src/components/slateReact.js
+++ src/components/slateReact.js
@@ -1226,7 +1226,7 @@ const CheckList = ({ attributes, children, element }) => {
 					</span>
 				<span
 				contentEditable={true}
-						style={{ paddingLeft: '2px' }}
+						style={{ paddingLeft: '5px' }}
 						className={css`
 					flex: 1;
 					opacity: ${checked ? 0.666 : 1};
