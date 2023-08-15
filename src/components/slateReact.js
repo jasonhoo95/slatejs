342097d0c7fabdbd79f84c469f177267a5a136d9
hoo chong yi <hoochongyi95@gmail.com>
2023-08-16 00:42:23 +0800

    done

--- src/components/slateReact.js
+++ src/components/slateReact.js
@@ -114,9 +114,8 @@ const SlateReact = () => {
 			if (event.data == "bold") {
 				toggleMark(editor, "bold");
 			} else if (event.data == "blur") {
-				window.removeEventListener("resize", getCaretCoordinates);
-				this.window.scrollTo(0, 0);
-				ReactEditor.blur(editor);
+				// this.window.scrollTo(0, 0);
+
 				window.flutter_inappwebview?.callHandler("handlerFooWithArgs", "blur1");
 			} else if (event.data == "katex") {
 				insertKatex(editor, "kkasdl", updateAmount);
