76cb33fc9e92305c3924f1c08a5f88662d7194af
hoo chong yi <hoochongyi95@gmail.com>
2023-10-14 14:12:13 +0800

    done

--- src/components/slateReact.js
+++ src/components/slateReact.js
@@ -724,36 +724,7 @@ const SlateReact = () => {
 
 						}
 
-						if (/android/i.test(ua)) {
-							if (string.text.startsWith("1.")) {
-								Editor.withoutNormalizing(editor, () => {
 
-									console.log(string.text, "text1");
-
-									const { selection } = editor;
-									const [start] = Editor.edges(editor, selection);
-									const path = start.path;
-
-									// Remove nodes at the current path
-									Transforms.removeNodes(editor, { at: path });
-									const block1 = { type: 'list-item', children: [{ text: '' }] }
-									Transforms.insertNodes(editor, { type: 'numbered-list', children: [block1] }, { at: path })
-									Transforms.unwrapNodes(editor, {
-										match: (n) => {
-											return !Editor.isEditor(n) && SlateElement.isElement(n) && (n.type == "paragraph");
-										},
-										at: path
-									});
-									const newPath = [...path, 0]; // Assuming you want to set the cursor at the start of the inserted node
-									Transforms.select(editor, Editor.range(editor, newPath));
-
-
-
-								})
-
-							}
-
-						}
 
 
 
@@ -809,6 +780,36 @@ const SlateReact = () => {
 
 
 						} 
+						if (/android/i.test(ua)) {
+							if (string.text.startsWith("1.")) {
+								Editor.withoutNormalizing(editor, () => {
+
+									console.log(string.text, "text1");
+
+									const { selection } = editor;
+									const [start] = Editor.edges(editor, selection);
+									const path = start.path;
+
+									// Remove nodes at the current path
+									Transforms.removeNodes(editor, { at: path });
+									const block1 = { type: 'list-item', children: [{ text: '' }] }
+									Transforms.insertNodes(editor, { type: 'numbered-list', children: [block1] }, { at: path })
+									Transforms.unwrapNodes(editor, {
+										match: (n) => {
+											return !Editor.isEditor(n) && SlateElement.isElement(n) && (n.type == "paragraph");
+										},
+										at: path
+									});
+									const newPath = [...path, 0]; // Assuming you want to set the cursor at the start of the inserted node
+									Transforms.select(editor, Editor.range(editor, newPath));
+
+
+
+								})
+
+							}
+
+						}
 
 
 					}}
