66e9e38db00ee43c5756024bcc41e81340864321
hoo chong yi <hoochongyi95@gmail.com>
2024-02-03 14:11:52 +0800

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,8 +37,8 @@ export default function Home() {
 			</Head>
 
 			{/* <SlateMobile /> */}
-			{/* <SlateReact /> */}
-			<SlateAndroid />
+			<SlateReact />
+			{/* <SlateAndroid /> */}
 			{/* <TablesExample /> */}
 
 			{/* {value['inputTxt']} */}
