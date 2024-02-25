26e22b604d2ca5492a2a7f9036247a73e5ad3276
hoo chong yi <hoochongyi95@gmail.com>
2024-02-25 20:28:41 +0800

    revert back code

--- src/components/slateReact.js
+++ src/components/slateReact.js
@@ -116,52 +116,52 @@ const SlateReact = () => {
 	const { insertBreak } = editor;
 
 
-	// useEffect(() => {
+	useEffect(() => {
 
-	// 	const messageListener = (event) => {
-	// 		console.log("message");
-	// 		if (event.data == "bold") {
-	// 			toggleMark(editor, "bold");
-	// 		} else if (event.data == "blur") {
+		const messageListener = (event) => {
+			console.log("message");
+			if (event.data == "bold") {
+				toggleMark(editor, "bold");
+			} else if (event.data == "blur") {
 
-	// 			ReactEditor.blur(editor);
-	// 			// this.window.scrollTo(0, 0);
-	// 		} else if (event.data == "katexinsert") {
-	// 			Transforms.insertText(editor, "\u200B".toString(), {
-	// 				at: editor.selection.anchor,
-	// 			});
+				ReactEditor.blur(editor);
+				// this.window.scrollTo(0, 0);
+			} else if (event.data == "katexinsert") {
+				Transforms.insertText(editor, "\u200B".toString(), {
+					at: editor.selection.anchor,
+				});
 
-	// 		}
-	// 		else if (event.data == "katex") {
-	// 			ReactEditor.focus(editor);
-
-
-	// 			insertKatex(editor, "flutter123");
+			}
+			else if (event.data == "katex") {
+				ReactEditor.focus(editor);
 
 
-	// 		} else if (event.data == "focus") {
-	// 			ReactEditor.focus(editor);
-	// 			// const parentCheck = Editor.parent(editor, editor.selection.anchor.path, { match: (n) => n.type == "katex" });
-	// 			// if (parentCheck[0].type == "katex") {
-	// 			// 	Transforms.move(editor, { distance: 1, unit: "offset" });
-	// 			// }
-	// 		} else {
-	// 			window.removeEventListener("message", messageListener);
-
-	// 		}
-	// 	};
-
-	// 	window.addEventListener("message", messageListener);
-
-	// 	return () => {
-	// 		window.removeEventListener("message", messageListener);
-	// 	};
-
-	// 	// Cleanup when the component unmounts or when the dependency changes
+				insertKatex(editor, "flutter123");
 
 
+			} else if (event.data == "focus") {
+				ReactEditor.focus(editor);
+				// const parentCheck = Editor.parent(editor, editor.selection.anchor.path, { match: (n) => n.type == "katex" });
+				// if (parentCheck[0].type == "katex") {
+				// 	Transforms.move(editor, { distance: 1, unit: "offset" });
+				// }
+			} else {
+				window.removeEventListener("message", messageListener);
 
-	// }, []);
+			}
+		};
+
+		window.addEventListener("message", messageListener);
+
+		return () => {
+			window.removeEventListener("message", messageListener);
+		};
+
+		// Cleanup when the component unmounts or when the dependency changes
+
+
+
+	}, []);
 
 	editor.insertBreak = () => {
 		const selectedLeaf = Node.leaf(editor, editor.selection.anchor.path);
