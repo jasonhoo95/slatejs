7616fbac5f97c8df9a3baa4a3f2d8cada1899c3f
hoo chong yi <hoochongyi95@gmail.com>
2024-07-07 10:44:15 +0800

    done added input checking

--- src/components/slateReact.js
+++ src/components/slateReact.js
@@ -22,6 +22,7 @@ import {
   Path,
   Descendant,
   Element as SlateElement,
+  Node as SlateNode,
   Text,
   Range,
   Node,
@@ -84,6 +85,19 @@ const initialValue = [
     ],
   },
 ];
+
+const SHORTCUTS = {
+  '1.': 'list-item',
+  '-': 'list-item',
+  '+': 'list-item',
+  '>': 'block-quote',
+  '#': 'heading-one',
+  '##': 'heading-two',
+  '###': 'heading-three',
+  '####': 'heading-four',
+  '#####': 'heading-five',
+  '######': 'heading-six',
+};
 function getCaretCoordinates(height) {
   let x = 0,
     y = 0;
@@ -128,9 +142,45 @@ const SlateReact = () => {
   const renderElement = useCallback((props) => <Element {...props} />, []);
   const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
   const editor = useMemo(() => withInlines(withReact(withHistory(createEditor()))), []);
-  const { deleteFragment, deleteBackward, onChange } = editor;
+  const { deleteFragment, deleteBackward, onChange, insertText } = editor;
 
   const { insertBreak } = editor;
+
+  const handleDOMBeforeInput = useCallback(
+    (e) => {
+      queueMicrotask(() => {
+        const pendingDiffs = ReactEditor.androidPendingDiffs(editor);
+
+        const scheduleFlush = pendingDiffs?.some(({ diff, path }) => {
+          if (!diff.text.endsWith(' ')) {
+            return false;
+          }
+
+          const { text } = SlateNode.leaf(editor, path);
+          const beforeText = text.slice(0, diff.start) + diff.text.slice(0, -1);
+          if (!(beforeText in SHORTCUTS)) {
+            return;
+          }
+
+          const blockEntry = Editor.above(editor, {
+            at: path,
+            match: (n) => SlateElement.isElement(n) && Editor.isBlock(editor, n),
+          });
+          if (!blockEntry) {
+            return false;
+          }
+
+          const [, blockPath] = blockEntry;
+          return Editor.isStart(editor, Editor.start(editor, path), blockPath);
+        });
+
+        if (scheduleFlush) {
+          ReactEditor.androidScheduleFlush(editor);
+        }
+      });
+    },
+    [editor],
+  );
 
   useEffect(() => {
     const messageListener = (event) => {
@@ -166,6 +216,50 @@ const SlateReact = () => {
 
     // Cleanup when the component unmounts or when the dependency changes
   }, []);
+
+  editor.insertText = (text) => {
+    const { selection } = editor;
+
+    if (text.endsWith(' ') && selection && Range.isCollapsed(selection)) {
+      const { anchor } = selection;
+      const block = Editor.above(editor, {
+        match: (n) => SlateElement.isElement(n) && Editor.isBlock(editor, n),
+      });
+      const path = block ? block[1] : [];
+      const start = Editor.start(editor, path);
+      const range = { anchor, focus: start };
+      const beforeText = Editor.string(editor, range) + text.slice(0, -1);
+      const type = SHORTCUTS[beforeText];
+
+      if (type) {
+        console.log(type, 'type enter');
+        Transforms.select(editor, range);
+
+        if (!Range.isCollapsed(range)) {
+          Transforms.delete(editor);
+        }
+
+        const newProperties = {
+          type,
+        };
+        Transforms.setNodes <
+          SlateElement >
+          (editor,
+          newProperties,
+          {
+            match: (n) => SlateElement.isElement(n) && Editor.isBlock(editor, n),
+          });
+
+        if (type === 'list-item') {
+          toggleBlock(editor, 'numbered-list', 'number');
+        }
+
+        return;
+      }
+    }
+
+    insertText(text);
+  };
 
   editor.insertBreak = () => {
     const selectedLeaf = Node.leaf(editor, editor.selection.anchor.path);
@@ -738,6 +832,7 @@ const SlateReact = () => {
           style={{ padding: '10px' }}
           ref={contentEditableRef}
           autoCapitalize={false}
+          onDOMBeforeInput={handleDOMBeforeInput}
           autoCorrect={false}
           spellCheck={false}
           onFocus={onFocus}
@@ -794,21 +889,65 @@ const SlateReact = () => {
             ) {
               event.preventDefault();
               Transforms.move(editor, { distance: 1, unit: 'offset' });
-            } else if (stringText.includes('1.') && listItems[0].type != 'list-item' && /android/i.test(ua)) {
-              Editor.withoutNormalizing(editor, () => {
-                toggleBlock(editor, 'numbered-list', 'number');
-                Transforms.delete(editor, {
-                  at: editor.selection.anchor,
-                  unit: 'word',
-                  reverse: true,
-                });
-              });
             }
           }}
         />
       </Slate>
     </div>
   );
+};
+
+const withShortcuts = (editor) => {
+  const { deleteBackward, insertText } = editor;
+
+  editor.insertText = (text) => {
+    const { selection } = editor;
+
+    if (text.endsWith(' ') && selection && Range.isCollapsed(selection)) {
+      const { anchor } = selection;
+      const block = Editor.above(editor, {
+        match: (n) => SlateElement.isElement(n) && Editor.isBlock(editor, n),
+      });
+      const path = block ? block[1] : [];
+      const start = Editor.start(editor, path);
+      const range = { anchor, focus: start };
+      const beforeText = Editor.string(editor, range) + text.slice(0, -1);
+      const type = SHORTCUTS[beforeText];
+
+      if (type) {
+        Transforms.select(editor, range);
+
+        if (!Range.isCollapsed(range)) {
+          Transforms.delete(editor);
+        }
+
+        const newProperties = {
+          type,
+        };
+        Transforms.setNodes <
+          SlateElement >
+          (editor,
+          newProperties,
+          {
+            match: (n) => SlateElement.isElement(n) && Editor.isBlock(editor, n),
+          });
+
+        if (type === 'list-item') {
+          const list = {
+            type: 'bulleted-list',
+            children: [],
+          };
+          Transforms.wrapNodes(editor, list, {
+            match: (n) => !Editor.isEditor(n) && SlateElement.isElement(n) && n.type === 'list-item',
+          });
+        }
+
+        return;
+      }
+    }
+
+    insertText(text);
+  };
 };
 
 const wrapperCheck = (editor) => {
