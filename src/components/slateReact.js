f2d3e1a6fbc9bbd25df8bbfd57ba809c0007a41d
hoo chong yi <hoochongyi95@gmail.com>
2024-11-23 16:33:22 +0800

    fixed table list drag  bugs

--- src/components/slateReact.js
+++ src/components/slateReact.js
@@ -1878,7 +1878,7 @@ const TableList = ({ attributes, children, element }) => {
           })}
         </tbody>
       </table>
-      <div contentEditable='false' style={{ display: selected ? 'block' : 'none' }} className='absolute right-0'>
+      <div contentEditable='false' style={{ display: selected ? 'block' : 'none' }} className='absolute right-0 h-0 w-0'>
         {children[0]}
       </div>
     </div>
